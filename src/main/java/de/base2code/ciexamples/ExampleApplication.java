33100ac45a38c2f502d9143f510da30f9c1f7867
base2code <niklas.grenningloh@gmx.de>
2022-11-16 15:21:59 +0100

    initial commit

---
+++ src/main/java/de/base2code/ciexamples/ExampleApplication.java
@@ -0,0 +1,13 @@
+package de.base2code.ciexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/de/base2code/ciexamples/HttpRequestTest.java
@@ -0,0 +1,27 @@
+package de.base2code.ciexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject(
+                "http://127.0.0.1:" + port + "/",
+                String.class)
+        ).contains("Hello World!");
+    }
+}
